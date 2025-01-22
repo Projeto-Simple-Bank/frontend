e3fa809f725c7ffa074d141bf837a38a96bc343c
Samira Grossi <grossisamira@hotmail.com>
2025-01-21 21:29:00 -0300

    feat: adiciona a estilização da sessão incial da home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { AreaInicialComponent } from './pages/home/sessao/area-inicial/area-inicial.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, AreaInicialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/pages/home/sessao/area-inicial/area-inicial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-inicial',
+  imports: [],
+  templateUrl: './area-inicial.component.html',
+  styleUrl: './area-inicial.component.css'
+})
+export class AreaInicialComponent {
+
+}
