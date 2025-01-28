30c9d8e80222d33e2d5532f35c84b1132b12c6ca
sophia albuquerque <sophiaalbuquerqueeleuterio@gmail.com>
2025-01-28 14:30:13 -0300

    feat: adcionado layout de dashboard cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages';
+import { DashboardClienteComponent } from './pages/dashboard-cliente/dashboard-cliente.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'deshboard', component: DashboardClienteComponent },
+];
---
+++ src/app/pages/dashboard-cliente/dashboard-cliente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-cliente',
+  imports: [],
+  templateUrl: './dashboard-cliente.component.html',
+  styleUrl: './dashboard-cliente.component.css'
+})
+export class DashboardClienteComponent {}
