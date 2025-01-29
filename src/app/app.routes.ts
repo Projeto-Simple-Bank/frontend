b11d14ffbd3c86e914c8b562657c73e198a4b8a7
Paula Vasconcelos <paulavasconcelos@gmail.com>
2025-01-29 10:14:05 -0300

    tela de login do admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages';
+import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [{ 
+    path: '', component: HomeComponent,  
+ },
+{
+    path: 'login-admin', component: LoginAdminComponent
+}];
