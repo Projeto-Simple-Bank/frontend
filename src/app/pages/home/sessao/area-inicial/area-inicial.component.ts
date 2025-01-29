06414b234a44e00e6c17057e9040b418d0879620
Samira Grossi <grossisamira@hotmail.com>
2025-01-29 15:15:35 -0300

    style: ajuste do nome das pastas e dos arquivos

---
+++ src/app/pages/home/sessao/area-inicial/area-inicial.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../../../components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-area-inicial',
+  imports: [SidebarComponent],
+  templateUrl: './area-inicial.component.html',
+})
+export class AreaInicialComponent {}
--- src/app/pages/home/sessao/areaInicial/areaInicial.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-area-inicial',
-  imports: [],
-  templateUrl: './areaInicial.component.html',
-})
-export class AreaInicialComponent {}
