5c68b009369559dca542dddc1418d6cdac256e88
Samira Grossi <grossisamira@hotmail.com>
2025-01-30 00:25:18 -0300

    feat: adiciona funcionalidade de envio de formulário para o backend dentro do cadastro-cliente

--- src/app/pages/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/pages/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from '../../services';
+import { Usuario } from '../../classes';
 
 @Component({
   selector: 'app-cadastro-cliente',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro-cliente.component.html',
 })
-export class CadastroClienteComponent {}
+export class CadastroClienteComponent {
+  constructor(private router: Router, private usuarioService: UsuarioService) {}
+
+  usuario: Usuario = new Usuario();
+
+  fechar(): void {
+    this.router.navigate(['/mensagem-cadastro']);
+  }
+
+  cadastrar(usuario: Usuario): void {
+    this.usuarioService.postUsuarioAPI(usuario).subscribe({
+      complete: () => this.fechar(),
+      error: (erro) => {
+        console.error(erro);
+        // window.alert(erro);
+      },
+    });
+  }
+}
