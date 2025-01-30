38abf2f2033f18fb0df477c28823df676728dd7e
Paula Vasconcelos <paulavasconcelos@gmail.com>
2025-01-30 09:03:45 -0300

    itens cadastro admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages';
 import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
+import { CadastroAdminComponent } from './components/cadastro-admin/cadastro-admin.component';
 
 export const routes: Routes = [{ 
     path: '', component: HomeComponent,  
- },
+},
 {
     path: 'login-admin', component: LoginAdminComponent
-}];
+},
+{
+    path: 'cadastro-admin', component: CadastroAdminComponent
+}
+];
