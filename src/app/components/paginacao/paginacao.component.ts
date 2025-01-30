6c1f1a806b43c5c23da057d07fca61ca855aa182
Samira Grossi <grossisamira@hotmail.com>
2025-01-29 23:31:41 -0300

    feat: adiciona o componente paginacao

---
+++ src/app/components/paginacao/paginacao.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginacao',
+  imports: [],
+  templateUrl: './paginacao.component.html',
+})
+export class PaginacaoComponent {}
