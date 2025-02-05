426d19b9afedfa351527f04cc627a1f06283f2e6
Samira Grossi <grossisamira@hotmail.com>
2025-02-04 22:56:18 -0300

    feat: adiciona a estilizacao de area-pix

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -3,3 +3,4 @@ export * from './login/login.component';
 export * from './dashboard-cliente/dashboard-cliente.component';
 export * from './transacao/transacao.component';
 export * from './cadastro-cliente/cadastro-cliente.component';
+export * from './area-pix/area-pix.component';
