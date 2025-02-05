2f0e52018b440ccc8c7986a7f4641c53f31e9296
Samira Grossi <grossisamira@hotmail.com>
2025-02-04 22:54:30 -0300

    feat: adiciona valor padrão em agência de conta

--- src/app/classes/conta.ts
+++ src/app/classes/conta.ts
@@ -4,7 +4,7 @@ export class Conta {
   constructor(
     public id: string = '',
     public tipoConta: number = 1,
-    public agencia: number = 0,
+    public agencia: number = 1001,
     public saldo: number = 0.0,
     public conta: string = '',
     public senha: string = '',
