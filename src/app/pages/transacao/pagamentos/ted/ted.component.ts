00ba07e2f0f55c90b4f5c555ad8cfd6178cdc121
Samira Grossi <grossisamira@hotmail.com>
2025-02-04 23:04:29 -0300

    feat: adiciona alerta de erro dentro do ted.component

--- src/app/pages/transacao/pagamentos/ted/ted.component.ts
+++ src/app/pages/transacao/pagamentos/ted/ted.component.ts
@@ -5,6 +5,7 @@ import { CardComponent } from '../../../../components';
 import { ContaService, TransacaoService } from '../../../../services';
 import { Conta, Transacao, Usuario } from '../../../../classes';
 import { formatarData } from '../../../../utils';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ted',
@@ -21,30 +22,33 @@ export class TedComponent {
   conta: Conta = new Conta();
   usuario: Usuario = new Usuario();
 
-  onChangeConta(numeroConta: string) {
-    console.log('Mudança no campo de conta:', numeroConta);
-  }
-
   consultarConta(numeroConta: string): void {
     this.contaService.getNumeroContaAPI(numeroConta).subscribe({
       next: (resp) => (this.conta = resp),
       error: (erro) => {
-        console.error('Erro ao consultar a conta:', erro);
+        console.error(erro);
       },
     });
   }
 
-  efetuarPagamento(transasacao: Transacao): void {
+  efetuarPagamento(transacao: Transacao): void {
     this.transacao.tipoTransacao = 2; // acho que já está enviando
     this.transacao.contaOrigem = sessionStorage.getItem('auth_token') as string;
     this.transacao.contaDestino = this.conta.id;
     this.transacao.dataTransacao = formatarData(new Date());
-    this.transacao.descricao = transasacao.descricao;
-    this.transacao.valor = transasacao.valor;
+    this.transacao.descricao = transacao.descricao;
+    this.transacao.valor = transacao.valor;
     this.transacaoService.postTransacaoAPI(this.transacao).subscribe({
       error: (erro) => {
         console.error(erro);
-        // window.alert(erro);
+        Swal.fire({
+          title:
+            'Não foi possível fazer essa transação verifique o saldo ou o número da conta.',
+          icon: 'error',
+          confirmButtonColor: '#e80070',
+          timer: 3000,
+          customClass: { title: 'alert' },
+        });
       },
     });
   }
